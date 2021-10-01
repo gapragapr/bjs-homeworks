4fdb24ec73464cac49f75a442a5e487cca6cb352
Gapragapr <gapragapr3@mail.ru>
2021-10-01 19:30:03 +0600

    add validation on data

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,8 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
-
-  "use strict";
 
   const discr = Math.pow(b, 2) - (4 * a * c);
   
@@ -25,20 +25,51 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-  "use strict";
   // код для задачи №2 писать здесь
+
+  const errors = [];
+
+  errors.push(validateInputValue('Процентная ставка', percent));
+  errors.push(validateInputValue('Начальный взнос', contribution));
+  errors.push(validateInputValue('Общая стоимость', amount));
+  errors.push(validateDateValue('Срок ипотеки', date));
+
+  const invalid = errors.filter(e => e !== '');
+  if (invalid.length){
+    return invalid.join('. ');
+  };
 
   let creditBody = amount - contribution;
   let creditTime = new Date();
   let difference = date - creditTime;
   let differenceMonth =  Math.floor((difference / 2592000000)); 
-  
-  p = percent / 12 / 100;
 
-  everyMonth = ((creditBody * (p + p / (((1 + p)**differenceMonth) - 1))));
+  const p = percent / 12 / 100;
+  const everyMonth = ((creditBody * (p + p / (((1 + p)**differenceMonth) - 1))));
+
   totalAmount = everyMonth * differenceMonth;
-  console.log(creditBody)
-  console.log(typeof percent + " " + typeof contribution + " " + typeof amount + " " + typeof date)
 
-  return totalAmount.toFixed(2);
+  return Number(totalAmount.toFixed(2));
+
+  function validateInputValue(inputName, inputValue){
+    if (inputValue === ''){
+      return `Параметр <${inputName}> не введен`;
+    }
+
+    const numberValue = Number(inputValue);
+
+    if (isNaN(numberValue)){
+      return `Параметр "${inputName}" содержит неправильное значение "${inputValue}"`;
+    }
+
+    return '';
+  }
+
+  function validateDateValue(inputName, inputValue){
+    if (isNaN(inputValue)){
+      return `Дата "${inputName}" не выбрана`
+    }
+
+    return '';
+  }
 }
