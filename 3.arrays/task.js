9dd9f4693167d5c0bf492d7f178eb46384be96b3
Gapragapr <gapragapr3@mail.ru>
2021-10-09 13:03:48 +0600

    add 2 quest

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  
   // Ваш код
-
+  
   return result; // boolean
-}
 
+}
 function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10);
+  
   return resultArr; // array
 }
