f4d9a95bc4cebe1d339505dcb02a732b464cc347
Gapragapr <gapragapr3@mail.ru>
2021-10-09 21:30:51 +0600

    update js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,28 @@
 function compareArrays(arr1, arr2) {
   let result;
-  
+
   // Ваш код
+  if (arr1.length === arr2.length){
+    arr1.every(function(element, index) {
+      if (arr1[element] === arr2[element]) {
+        result = 1;
+      }
+      else{
+        result = 0;
+      }
+    })
+  }
+  else{
+    result = 0;
+  }
   
-  return result; // boolean
+  return Boolean(result); // boolean
 
 }
 function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-  resultArr = arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10);
-  
-  return resultArr; // array
+
+  return resultArr = arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10); // array
 }
