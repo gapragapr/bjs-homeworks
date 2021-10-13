4c3c7bd072b91dc66b7506d50fc3f114091601e1
Gapragapr <gapragapr3@mail.ru>
2021-10-13 17:13:17 +0600

    update js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
     // Ваш код
-
+      this.name = name;
+      this.gender = gender;
+      this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  if (this.subject === undefined){
+    this.subject = subjectName;
+  }
 }
 
+Student.prototype.addMark = function (...mark) {
+  if (this.marks === undefined){
+    this.marks = [...mark];
+  }
+  else{
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.exclude = function (reason){
+  if (this.excluded === undefined){
+    delete this.subject; 
+    delete this.marks;
+    this.excluded = reason;
+  }
+  
+}
 // ваш код для остальных методов
+
+Student.prototype.getAverage = function (){
+  
+}
