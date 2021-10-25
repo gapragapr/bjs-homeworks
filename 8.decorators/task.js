d3ed56a948f69d644c6eedeaf92cb7ef6f2888a9
Gapragapr <gapragapr3@mail.ru>
2021-10-26 03:20:34 +0600

    update JS

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,55 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let array = [];
+  let cache = {};
+
+  return function (...args) {
+    const hash = args.join(',');
+    if (hash in cache){
+      return `Из кэша: ${cache[hash]}`;
+    }
+    if (array.length >= 5){
+      delete cache[array.shift()];
+    }
+    array.push(hash);
+    cache[hash] = func(...args);
+    return `Вычисляем: ${cache[hash]}`;
+  }
 }
 
 
+
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timerID;
+  let isWait = false;
+
+  return function(){
+    if (!isWait){
+      func();
+      isWait = true;
+    }
+    else{
+      clearInterval(timerID);
+      console.log(`Подождите немного и попробуйте снова`)
+    }
+    timerID = setTimeout(() => {func(), isWait = false;}, ms)
+  }
 }
 
 function debounceDecorator2(func) {
   // Ваш код
+  let count = 0;
+  let isWait = false;
+
+  function sendMessage() {
+    console.log(isWait)
+    if (!isWait) {
+      func();
+      sendMessage.counter = ++count;
+      setTimeout(()=> { func(); sendMessage.counter = ++count; isWait = false; }, ms);
+      isWait = true;
+    }
+  }
+  sendMessage.counter = 0;
+
+  return sendMessage;
 }
